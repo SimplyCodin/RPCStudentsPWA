2f892bc29d93e0d4e30d51ee5a90c9af90fdfcd1
Simply Codin <SimplyCodin@users.noreply.github.com>
2017-11-19 09:34:41 -0600

    Fixed Obscure Building Problem

---
+++ src/components/Pais.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="pais">
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'pais'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+	.pais
+		height: 100%
+		width: 100%
+		background-image: url("https://scontent-dft4-3.xx.fbcdn.net/v/t1.0-9/21430535_1614286931979664_8055246073799756892_n.jpg?oh=063041d6a38d0beec4dcb70e6d0dbd63&oe=5A542C58")
+</style>
