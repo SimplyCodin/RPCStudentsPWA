36aab7fb227d6ae77a5e752e04ef1aca55007876
Simply Codin <SimplyCodin@users.noreply.github.com>
2018-01-07 19:49:52 -0600

    Moved Assets

--- poi.config.js
+++ poi.config.js
@@ -2,6 +2,7 @@ const OfflinePlugin = require('offline-plugin')
 
 module.exports = options => ({
 	templateCompiler: true,
+	copy:{from:"assets",to:"dist/assets"},
 	extendWebpack(config) {
 		if (process.env.NODE_ENV === 'production'){
 			config.plugin('offline')
