cc1e1e31ac5271066e37114d451187cc5f10b448
Simply Codin <SimplyCodin@users.noreply.github.com>
2018-01-08 18:05:26 -0600

    Update Manifest

--- poi.config.js
+++ poi.config.js
@@ -2,7 +2,7 @@ const OfflinePlugin = require('offline-plugin')
 
 module.exports = options => ({
 	templateCompiler: true,
-	copy:[{from:"assets",to:"assets"},{from:"static",to:""}],
+	copy:[{from:"assets",to:"assets"},{from:"static",to:"static"}],
 	extendWebpack(config) {
 		if (process.env.NODE_ENV === 'production'){
 			config.plugin('offline')
